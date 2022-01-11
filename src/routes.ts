f75f17ee69c147f878f44c9788fb94c10fa1c196
RobertSancts <hdx_sancts@hotmail.com>
2022-01-11 16:53:41 -0300

    criando o arquivo de rota e importando para o arquivo server.ts

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { CreateClientController } from "./modules/useCase/createClient/CreateClientController";
+
+const routes = Router();
+
+const createClientController = new CreateClientController();
+
+
+routes.post("/client/", createClientController.handle);
+
+export { routes }
+
+
